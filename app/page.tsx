0b1c7b415fb903262fbe2692736da7210f3998d0
Ppantaleo <patriciopantaleo@hotmail.com>
2025-07-15 20:54:30 -0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function HomePage() {
       <Hero />
       <Stats />
       <Services />
-      <DroneSection />
       <AISection />
+      <DroneSection />
       <About />
       <CTA />
     </main>
